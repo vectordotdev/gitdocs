3945253d4384fae33d7cb862a72b3a628334008e
etimberg <evert.timberg@gmail.com>
2017-12-18 19:59:18 -0500

    Init command

--- bin/gitdocs.js
+++ bin/gitdocs.js
@@ -3,11 +3,14 @@
 const chalk = require('chalk')
 const execSync = require('child_process').execSync
 const fs = require('fs-extra')
+const inquirer = require('inquirer')
 const isGlobal = require('is-global')
+const merge = require('lodash.merge')
 const path = require('path')
 const yargs = require('yargs')
 
 const cwd = process.cwd()
+const defaults = require('../default.json')
 
 // 3 possible cases:
 //    Global install - need to go up and into node modules to find react static
@@ -80,6 +83,99 @@ var argv = yargs
 
       const distDir = path.join(cwd, 'dist')
       fs.copySync(distDir, argv.output)
+    }
+  })
+  .command({
+    command: 'init',
+    alias: 'i',
+    desc: chalk.gray('init'),
+    handler: async argv => {
+      console.log(chalk.green('Initializing GitDocs project'))
+
+      const docs = path.join(cwd, 'docs')
+      const public = path.join(docs, 'public')
+
+      try {
+        console.log(`Creating doc directory: ${docs}`)
+        fs.mkdirSync(docs)
+      } catch (err) {
+        if (err.code !== 'EEXIST') {
+          console.error(chalk.red('Could not create doc directory'))
+          throw err
+        } else {
+          console.warn(chalk.yellow('Docs directory already exists.'))
+        }
+      }
+
+      try {
+        console.log(`Creating public directory: ${public}`)
+        await fs.mkdir(public)
+      } catch (err) {
+        if (err.code !== 'EEXIST') {
+          console.error(chalk.red('Could not create public directory'))
+          throw err
+        } else {
+          console.warn(chalk.yellow('Public directory already exists.'))
+        }
+      }
+
+      // Create default config & welcome file
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What should we name this project?',
+          default: 'my-documentation',
+        },
+        {
+          type: 'input',
+          name: 'repository',
+          message: 'URL of the repository where this documentation is stored.'
+        }
+      ])
+
+      const configFile = path.join(docs, 'docs.json')
+      const config = merge(defaults, {
+        name: answers.name,
+        repository: answers.repository,
+        title: answers.name,
+        version: '0.0.1',
+        sidebar: {
+          items: {
+            'Introduction': 'introduction.md'
+          }
+        }
+      })
+
+      try {
+        console.log(`Writing default config file as ${configFile}`)
+        const fp = await fs.open(configFile, 'wx')
+        await fs.write(fp, JSON.stringify(config, null, 2))
+      } catch (err) {
+        if (err.code !== 'EEXIST') {
+          console.error(chalk.red('Could not write config file.'))
+          throw err
+        } else {
+          console.warn(chalk.yellow('docs.json file already exists and was left unchanged.'))
+        }
+      }
+
+      const readme = path.join(__dirname, '../init/introduction.md')
+      const outputReadme = path.join(docs, 'introduction.md')
+      try {
+        console.log(`Writing default readme as ${outputReadme}`)
+        await fs.copy(readme, outputReadme, {
+          overwrite: false,
+          errorOnExist: true
+        })
+      } catch (err) {
+        if (err.code !== 'EEXIST') {
+          console.error(chalk.red('Could not write readme file.'))
+          throw err
+        } else {
+          console.warn(chalk.yellow('Readme file already exists and was left unchanged.'))
+        }
+      }
     }
   })
   .demand(1, "must provide a valid command")
---
+++ init/introduction.md
@@ -0,0 +1,41 @@
+# Welcome to GitDocs
+
+Easy to use, SEO-friendly, beautiful documentation that lives in your git repo.
+
+## How does it work?
+
+It turns markdown files in your `/docs` folder (or wherever you choose) into rich, interactive documentation. Out of the box you get helpers like syntax highlighting using [Prism](prismjs.com) or [Highlight.js](https://highlightjs.org/), callouts like [Tip](writing/helpers#tip), <Highlight>highlight</Highlight> and much more. You can even extend GitDocs to support custom components using libraries like React and Vue.
+
+`gitdocs` is distributed as a package on npm. It is a cli that you can simply install with `npm install gitdocs -g`. You can run `gitdocs serve` to preview your docs or `gitdocs build` to generate a static site wherever you like.
+
+## Installation
+
+Using npm:
+
+```bash
+npm i -g gitdocs
+```
+
+Using Yarn:
+
+```bash
+yarn global add gitdocs
+```
+
+## Quick Start
+
+Assuming you have a folder with markdown files at `/docs`, you can automatically preview your docs site by running:
+
+```bash
+gitdocs serve
+```
+
+and build your site to any destination with:
+
+```bash
+gitdocs build --output="docs-dist"
+```
+
+- The `readme.md` in your repo will be used as the default homepage for your docs
+- `docs/docs.json` allows you to specify your project settings like the version, name and much more
+- The navigation will automatically be built using your docs folder structure, but you can override this in `docs.json` to generate your own navigation.
