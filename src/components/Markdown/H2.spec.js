a6f8a268de9f1e5b857746d6033b5eb21bd5f194
etimberg <evert.timberg@gmail.com>
2017-12-21 11:21:38 -0500

    Component tests

---
+++ src/components/Markdown/H2.spec.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Header from './H2'
+
+describe('Component: H2', () => {
+    let h2
+
+    beforeAll(() => {
+        h2 = shallow(<Header id="myId">Hello!</Header>)
+    })
+
+    test('Should be constructed', () => {
+        expect(h2).not.toBe(undefined)
+    })
+
+    test('Should render an anchor', () => {
+        const a = h2.find('#myId')
+        expect(a).not.toBe(undefined)
+        expect(a.text()).toBe('Hello!')
+    })
+})
