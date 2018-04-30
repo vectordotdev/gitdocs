41356a09f1c350c901cb42e09c405f7ec77fc1f4
Jason Maurer <j@maur.co>
2018-04-30 15:07:50 -0400

    pkg standalone binaries

--- tests/help.test.js
+++ tests/help.test.js
@@ -3,15 +3,15 @@ import { run } from './helpers'
 
 test('shows help menu with command', async t => {
   const res = await run('help')
-  t.regex(res.stderr, /usage/)
+  t.regex(res.stdout, /usage/)
 })
 
 test('shows help submenu with command', async t => {
   const res = await run('help start')
-  t.regex(res.stderr, /gitdocs start/)
+  t.regex(res.stdout, /gitdocs start/)
 })
 
 test('shows help submenu with flag', async t => {
   const res = await run('start -h')
-  t.regex(res.stderr, /gitdocs start/)
+  t.regex(res.stdout, /gitdocs start/)
 })
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,16 +3,16 @@ import { run } from './helpers'
 
 test('defaults to help command', async t => {
   const res = await run()
-  t.regex(res.stderr, /usage/)
+  t.regex(res.stdout, /usage/)
+})
+
+test('shows package version', async t => {
+  const res = await run('-v')
+  t.regex(res.stdout, /v[0-9]/)
 })
 
 test('throws on invalid command', async t => {
   const res = await run('foobar', true)
   t.is(res.code, 1)
   t.regex(res.stderr, /not a valid/)
-})
-
-test('shows package version', async t => {
-  const res = await run('-v')
-  t.regex(res.stderr, /v[0-9]/)
 })
