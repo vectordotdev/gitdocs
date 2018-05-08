2bb86ee885f337a524fe1a29451548a4035806ea
Zach Sherman <zksherm@gmail.com>
2018-05-08 14:04:31 -0400

    remove old dist folder

--- dist/bundler/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.DEFAULT_PORT = void 0;
-const DEFAULT_PORT = process.env.PORT || 3000;
-exports.DEFAULT_PORT = DEFAULT_PORT;
