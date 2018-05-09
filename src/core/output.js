b266f3be3fa890b64539783921e9e338577ac50f
Jason Maurer <j@maur.co>
2018-05-09 16:27:12 -0400

    static assets during build

--- src/core/output.js
+++ src/core/output.js
@@ -3,15 +3,24 @@ const renderTemplate = require('./template')
 const { concurrentChunks } = require('../utils/promise')
 
 module.exports = async (env, stats, props, bar) => {
-  const { files } = props.manifest
+  const {
+    manifest,
+    config: {
+      temp,
+      output,
+    },
+  } = props
+
   const bundleFiles = stats.entrypoints.main.assets
 
-  await concurrentChunks(2, files.map(item => async () => {
+  await fs.copy(`${temp}/@static`, output)
+
+  await concurrentChunks(2, manifest.files.map(item => async () => {
     const rendered = await renderTemplate(env, item, props, bundleFiles)
     await fs.outputFile(item.fileOutput, rendered)
 
     bar && bar.tick()
   }))
 
-  return props.config.output
+  return output
 }
