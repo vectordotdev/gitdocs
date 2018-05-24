99579ed06a7203cf3c873ccd145030e4397d80c1
Jason Maurer <j@maur.co>
2018-05-24 13:10:18 -0400

    output a db.json for server and output

--- src/core/database.js
+++ src/core/database.js
@@ -1,7 +1,30 @@
-// async function generateDatabase (tree) {
-//
-// }
+const { getContent } = require('./filesystem')
+
+async function generateDatabase (manifest) {
+  const db = []
+
+  const _recursive = async ({ items, ...item }, bc = []) => {
+    if (item.input) {
+      db.push({
+        url: item.url,
+        breadcrumb: bc,
+        title: item.title,
+        content: await getContent(item.input),
+      })
+    }
+
+    if (items) {
+      const breadcrumb = bc.concat(item.title)
+      await Promise.all(
+        items.map(i => _recursive(i, breadcrumb))
+      )
+    }
+  }
+
+  await _recursive(manifest)
+  return db
+}
 
 module.exports = {
-  // generateDatabase,
+  generateDatabase,
 }
