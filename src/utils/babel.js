3fc5048f70cab352d0ef9bc3f43e26505a484a9d
Jason Maurer <j@maur.co>
2018-05-24 16:04:01 -0400

    fix babel issues

--- src/utils/babel.js
+++ src/utils/babel.js
@@ -1,29 +1,21 @@
-const syspath = require('path')
-
-function getMod (path, namespaced = true) {
-  const baseDir = syspath.resolve(__dirname, '../../node_modules')
-  const module = require(`${baseDir}/babel-${path}`)
-
-  return module.default || module
-}
-
 function babelOptions (forServer) {
   return {
     babelrc: false,
     presets: [
-      getMod('preset-env'),
-      getMod('preset-react'),
+      require.resolve('babel-preset-env'),
+      require.resolve('babel-preset-react'),
     ],
     plugins: [
-      getMod('plugin-transform-runtime'),
-      getMod('plugin-transform-object-rest-spread'),
-      getMod('plugin-transform-class-properties'),
+      require.resolve('babel-plugin-transform-runtime'),
+      require.resolve('babel-plugin-transform-object-rest-spread'),
+      require.resolve('babel-plugin-transform-class-properties'),
     ].filter(Boolean),
   }
 }
 
 function babelRequire (path) {
   const opts = babelOptions(true)
+  opts.ignore = false
 
   require('babel-register')(opts)
   return require(path)
