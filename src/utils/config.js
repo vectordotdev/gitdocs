c35addcc99eba23f7a86de5f06f60ac0c02c70a4
Jason Maurer <j@maur.co>
2018-06-04 17:21:48 -0400

    automatically disable page titles

--- src/utils/config.js
+++ src/utils/config.js
@@ -28,7 +28,7 @@ const DEFAULT_CONFIG = {
   languages: ['bash', 'json'],
   header_links: [],
   theme: 'default',
-  prefix_titles: true,
+  prefix_titles: false,
   table_of_contents: {
     page: true,
     folder: true,
